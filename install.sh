b8f676b5c8c0216f1663f78d73dc66dc04a363e9
choozn <65042105+choozn@users.noreply.github.com>
2025-07-20 21:30:51 +0200

    fixup! all: initialize project

--- install.sh
+++ install.sh
@@ -2,10 +2,12 @@
 # Copyright (C) 2025 choozn
 # Installation script for my dotfiles
 # It is recommended to install on a fresh installation of Arch Linux using systemd.
-# Using a different distro and/or init system might lead to unwanted problems.
+# Using a different distro and/or init system will lead to unwanted problems.
 #
 # To install in one line, run the following command:
 # curl -Ls dots.choozn.dev | bash
+
+source ./whisker/main
 
 check_root() {
     if [[ $EUID -eq 0 ]]; then
@@ -28,25 +30,14 @@ EOF
     echo "[!] To install the dotfiles and other dependencies, I'll need you to give me sudo privileges."
     echo -e "[!] Don't worry, I'm a well-behaved kitty! No messes, just magic.\n"
 }
-
-# Installation config
-install_name="whisker"
-install_path="$HOME/${install_name}"
-backup_root="$HOME/${install_name}_backups"
-backup_folder="${backup_root}/${install_name}_backup_$(date +'%Y-%m-%d_%H-%M-%S')"
-
-# Whisker config
-bundle_folder="bundles"
-bundle_config="bundle.conf"
-
+#
 # Install Steps
 check_root
 greet
-
-source ./whisker/main
 whisker
 
-echo -e "[!] Installation of all mandatory dependencies successful!"
+# Done
+echo -e "[!] Installation of successful!"
 
 # Start Hyprland
 if [[ "$XDG_CURRENT_DESKTOP" != "Hyprland" ]]; then
